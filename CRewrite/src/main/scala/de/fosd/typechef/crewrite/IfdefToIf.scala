8fb115fceee3181f058201ed95267d9fd79fcf4b
rhein <rhein@fim.uni-passau.de>
2013-08-13 17:13:10 +0200

    fixed missing variability elimination in DeclarationInit

--- CRewrite/src/main/scala/de/fosd/typechef/crewrite/IfdefToIf.scala
+++ CRewrite/src/main/scala/de/fosd/typechef/crewrite/IfdefToIf.scala
@@ -863,6 +863,7 @@ class IfdefToIf extends ASTNavigation with ConditionalNavigation {
                         List(Opt(trueF, o.entry))
                     } else {
                         // TODO: To list(o) or not
+                        logger.error("Could not eliminate variability (non-exhaustive ifdef in file?): " + o.toString)
                         List(o)
                     })
             case i: Id =>
@@ -1733,7 +1734,10 @@ class IfdefToIf extends ASTNavigation with ConditionalNavigation {
                 case d@Opt(ft, entry: ParameterDeclarationD) =>
                     if (ft.equals(trueF) || ft.equals(FeatureExprFactory.False)) entry.productIterator.toList.flatMap(getNextFeatureHelp(_, currentContext)) else List(fixTypeChefsFeatureExpressions(ft, currentContext)) ++ entry.productIterator.toList.flatMap(getNextFeatureHelp(_, fixTypeChefsFeatureExpressions(ft, currentContext)))
                 case d@Opt(ft, entry: InitDeclaratorI) =>
-                    (if (!ft.equals(trueF) || ft.equals(FeatureExprFactory.False)) List(fixTypeChefsFeatureExpressions(ft, currentContext)) else List()) ++ entry.declarator.productIterator.toList.flatMap(getNextFeatureHelp(_, fixTypeChefsFeatureExpressions(ft, currentContext))) ++ entry.attributes.productIterator.toList.flatMap(getNextFeatureHelp(_, fixTypeChefsFeatureExpressions(ft, currentContext)))
+                    (if (!ft.equals(trueF) || ft.equals(FeatureExprFactory.False)) List(fixTypeChefsFeatureExpressions(ft, currentContext)) else List()) ++
+                        entry.declarator.productIterator.toList.flatMap(getNextFeatureHelp(_, fixTypeChefsFeatureExpressions(ft, currentContext))) ++
+                        entry.attributes.productIterator.toList.flatMap(getNextFeatureHelp(_, fixTypeChefsFeatureExpressions(ft, currentContext))) ++
+                        entry.i.productIterator.toList.flatMap(getNextFeatureHelp(_, fixTypeChefsFeatureExpressions(ft, currentContext)))
                 case d@Opt(ft, entry) =>
                     if (!ft.equals(trueF) || ft.equals(FeatureExprFactory.False)) List(fixTypeChefsFeatureExpressions(ft, currentContext)) else List()
                 case l: List[_] =>
