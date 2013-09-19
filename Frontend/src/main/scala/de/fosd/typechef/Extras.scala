2b8d7527fe6782b899a952abae93a8a5998149ab
rhein <rhein@fim.uni-passau.de>
2013-09-19 13:53:41 +0200

    better algorithm for detection of cfg features

--- Frontend/src/main/scala/de/fosd/typechef/Extras.scala
+++ Frontend/src/main/scala/de/fosd/typechef/Extras.scala
@@ -1,12 +1,18 @@
 package de.fosd.typechef
 
+import conditional.Choice
+import conditional.Opt
 import conditional.{Choice, Opt}
 import crewrite.{IfdefToIf, CAnalysisFrontend}
 import featureexpr.sat.SATFeatureModel
 import featureexpr.{FeatureModel, SingleFeatureExpr}
 import java.io.{File, FileWriter}
 import options.FrontendOptions
-import parser.c.{FunctionDef, TranslationUnit, AST}
+import parser.c._
+import parser.c.FunctionDef
+import parser.c.PostfixExpr
+import parser.c.TranslationUnit
+import scala.Some
 
 /**
  * Created with IntelliJ IDEA.
@@ -76,15 +82,27 @@ object Extras {
     }
 
     def getSimpleCFGfeatures(ast:AST, fm :FeatureModel) : Set[SingleFeatureExpr] = {
-        def getCalledFunctions(ast:AST, knownFunctionNames:Set[String]) : Set[String] = {
-            // for now, make it simple
-            // I only search for occurences of the function names I know
-            val astString = ast.toString
-            var ret :Set [String] = Set()
-            for (fname <- knownFunctionNames)
-                if (astString.contains(fname))
-                    ret = ret +fname
-            ret
+        def getCalledFunctions(ast:Any) : Set[String] = {
+            ast match {
+                case PostfixExpr(Id(fname:String), FunctionCall(exprs)) => Set(fname) ++ getCalledFunctions(exprs)
+                case x: Opt[_] => getCalledFunctions(x.entry)
+                case x: Choice[_] => getCalledFunctions(x.thenBranch) ++ getCalledFunctions(x.elseBranch)
+                case l: List[_] => {
+                    var ret: Set[String] = Set()
+                    for (x <- l) {
+                        ret = ret ++ getCalledFunctions(x)
+                    }
+                    ret
+                }
+                case x: Product => {
+                    var ret: Set[String] = Set()
+                    for (y <- x.productIterator.toList) {
+                        ret = ret ++ getCalledFunctions(y)
+                    }
+                    ret
+                }
+                case o => Set()
+            }
         }
         def getUsedFeaturesRec(fName:String,
                                functionToCalledFunctionsMap:Map[String,Set[String]],
@@ -96,7 +114,7 @@ object Extras {
                 val thisScannedFunctions = scannedFunctions + fName
                 var usedFeatures = getAllFeaturesRec(functionToASTMap.get(fName))
                 functionToCalledFunctionsMap.get(fName) match {
-                    case None => println ("no called function found for function " + fName)
+                    case None => println ("no function definition found for called function " + fName)
                     case Some(calledFunctions) => {
                         for (calledF : String <- calledFunctions) {
                             usedFeatures = usedFeatures ++ getUsedFeaturesRec(calledF, functionToCalledFunctionsMap, functionToASTMap, thisScannedFunctions)
@@ -109,12 +127,12 @@ object Extras {
 
         val cf = new CAnalysisFrontend(ast.asInstanceOf[TranslationUnit], fm)
         val lst : List[FunctionDef] = cf.filterAllASTElems[FunctionDef](ast)
-        val functionNames : Set[String] = lst.map(x => x.declarator.getId.name).toSet
+        //val functionNames : Set[String] = lst.map(x => x.declarator.getId.name).toSet
         var functionToCalledFunctionsMap : Map[String, Set[String]] = Map()
         var functionToASTMap : Map[String, FunctionDef] = Map()
         for (x: FunctionDef <- lst) {
             // problem: function overloading is not handled
-            functionToCalledFunctionsMap = functionToCalledFunctionsMap.updated(x.declarator.getId.name, getCalledFunctions(x, functionNames))
+            functionToCalledFunctionsMap = functionToCalledFunctionsMap.updated(x.declarator.getId.name, getCalledFunctions(x))
             functionToASTMap = functionToASTMap.updated(x.declarator.getId.name, x)
         }
         val startFunction:String = "ldv_main0_sequence_infinite_withcheck_stateful"
