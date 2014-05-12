b42136da5d79b1cc2dd22efdd9ec73f8e7aec4d7
Florian Garbe <f.garbe@gmx.de>
2014-05-12 13:52:37 +0200

    changed condition to for declUse from useFeature.implies(declarationFeature)isTautology to !useFeature.and(declarationFeature).isContradiction

--- CTypeChecker/src/main/scala/de/fosd/typechef/typesystem/CDeclUse.scala
+++ CTypeChecker/src/main/scala/de/fosd/typechef/typesystem/CDeclUse.scala
@@ -688,15 +688,13 @@ trait CDeclUse extends CDeclUseInterface with CEnv with CEnvCache {
         entry match {
             case use@Id(name) => {
                 if (env.structEnv.someDefinition(name, isUnion)) {
-                    println(env.structEnv.getId(name, isUnion))
-                    print("")
                     env.structEnv.getId(name, isUnion) match {
                         case o@One(key: Id) =>
                             addOne(o, use)
                         case c@Choice(_, _, _) =>
                             val condTuple = c.toList
                             // TODO
-                            val tuple = condTuple.filter(x => x._1.equivalentTo(FeatureExprFactory.True) || feature.and(x._1).isSatisfiable())
+                            val tuple = condTuple.filter(x => x._1.equivalentTo(FeatureExprFactory.True) || !feature.and(x._1).isContradiction())
                             val tupleVariableDef = condTuple.filter(x => x._1.implies(feature).isTautology).diff(tuple)
                             if (feature.implies(tupleVariableDef.foldLeft(FeatureExprFactory.False)((a, b) => a.or(b._1))).isTautology) {
                                 tupleVariableDef.foreach(x => {
