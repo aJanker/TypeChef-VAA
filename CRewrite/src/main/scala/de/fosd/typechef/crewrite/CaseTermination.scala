51a30948dab8e77beee738cadc19f3cd1c0796ee
ajanker <janker@fim.uni-passau.de>
2015-05-27 15:31:52 +0200

    Bugfix for non terminating recursion in case statement analysis

--- CRewrite/src/main/scala/de/fosd/typechef/crewrite/CaseTermination.scala
+++ CRewrite/src/main/scala/de/fosd/typechef/crewrite/CaseTermination.scala
@@ -1,7 +1,11 @@
 package de.fosd.typechef.crewrite
 
+import java.util
+
 import de.fosd.typechef.parser.c._
 import de.fosd.typechef.conditional.Opt
+
+import scala.reflect.internal.util.Collections
 
 // implements a simple analysis that checks whether a case statement associated with a statement
 // terminates under all conditions with a break statement
@@ -13,12 +17,15 @@ class CaseTermination(env: ASTEnv) extends IntraCFG {
         // and filter out other case statements, as fall through (case after case)
         // is allowed in this analysis
         var wlist: List[Opt[AST]] = succ(c, env).filterNot({
-            case Opt(_, _: CaseStatement) => true
+            case Opt(_, c@CaseStatement(_)) => true
             case _ => false
         })
 
         // determine switch to make sure we do not leave the successor element
         val switch = findPriorASTElem[SwitchStatement](c, env)
+
+        // store already visited condition statements of loops to avoid endless recursion
+        var loopCondition: List[AST] = List()
 
         // determine starting from the case statement that all successor elements will finally
         // come through a break statement
@@ -30,9 +37,21 @@ class CaseTermination(env: ASTEnv) extends IntraCFG {
                 case Opt(_, _: BreakStatement) =>
                 case Opt(_, _: CaseStatement) => return false
                 case Opt(_, _: DefaultStatement) => return false
-                case Opt(_, s) => if (!isPartOf(s, switch)) return false
-                else wlist ++= succ(s, env)
-
+                case Opt(_, s) =>
+                    if (!isPartOf(s, switch)) return false
+                    else if (!loopCondition.contains(s)) {
+                        parentAST(s, env) match {
+                            case f : ForStatement => loopCondition ::= s
+                            case w : WhileStatement => loopCondition ::= s
+                            case d : DoStatement => loopCondition ::= s
+                            case _ =>
+                        }
+                        wlist ++= succ(s, env)
+                }
+            }
+            if (wlist.size > 25000) {
+                println("Error - hit endless recursion for " +  curelem + " in " + curelem.entry.getPositionFrom)
+                return false;
             }
         }
 
